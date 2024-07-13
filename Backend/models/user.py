35b91b645885d79220dfa91e19e0308cffc549ad
mat05 <matary05@outlook.fr>
2024-07-13 12:36:32 +0200

    [1.0.0] - Add user class to test the CI workflow

---
+++ Backend/models/user.py
@@ -0,0 +1,25 @@
+from Backend.core.interfaces.interfaces import IUser
+
+
+class User(IUser):
+    def __init__(self, user_id, username, email, password):
+        self.user_id = user_id
+        self.username = username
+        self.email = email
+        self.password = password
+
+    def get(self, user_id):
+        # Implémentation pour récupérer un utilisateur par son ID
+        pass
+
+    def create(self, user):
+        # Implémentation pour créer un nouvel utilisateur
+        pass
+
+    def update(self, **kwargs):
+        # Implémentation pour mettre à jour un utilisateur par son ID
+        pass
+
+    def delete(self, user_id):
+        # Implémentation pour supprimer un utilisateur par son ID
+        pass
